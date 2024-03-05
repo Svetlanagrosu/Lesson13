987632628fb40e1295ff7c744d6619c694b78b55
Svetlanagrosu <grosu.svetlana.miha@gmail.com>
2024-03-05 22:29:00 +0100

    n commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const num = 8
+
+console.log(num, typeof num)
+
+let convertedNum = String(num)
+
+console.log(convertedNum, typeof convertedNum)
+
+let convertedStr = Number(convertedNum)
+
+console.log(typeof+convertedNum)
+
+let convertBoll = Boolean(convertedStr)
+
+console.log(convertBoll)
