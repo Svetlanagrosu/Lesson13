07679584a191b615304d74439734851b0c1d8849
Svetlanagrosu <grosu.svetlana.miha@gmail.com>
2024-03-05 20:13:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const autoRallySaffaryClub = 'str' (camelCaseNaimingConvetionType)
+
+
+const ELEMENTO_NOME_PPROPRIETA_CHIAVE_TAVOLA_TYPE = 'ELEMENTO_NOME_PROPRIETA_CHIAVE_TAVOLA-TYPE'
+console.log(ELEMENTO_NOME_PPROPRIETA_CHIAVE_TAVOLA_TYPE) 
+
+
+const Economy = new Object
