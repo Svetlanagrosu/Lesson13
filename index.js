ea9581551512702ea18c834c2f32b15128e13c9d
Svetlanagrosu <grosu.svetlana.miha@gmail.com>
2024-03-06 06:25:43 +0100

    n commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const falsy1 = false
+const falsy2 = NaN
+const falsy3 = null
+const falsy4 = ' '
+const falsy5 = 0
+const falsy6 = undefined
+
+
+
+const valueAnd = falsy1 || falsy2 || falsy3 || 'str'
+
+console.log( falsy1 || falsy2 || falsy3 || 888)
+
+
+
+
+
+
+
+
+
 
